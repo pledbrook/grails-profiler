774b18a02c168c2ce3068888df5732e328976d79
Tom Dunstan <github@tomd.cc>
2012-12-22 16:24:39 +0000

    remove some left over debugging

--- src/java/com/linkedin/grails/profiler/ClassUtil.java
+++ src/java/com/linkedin/grails/profiler/ClassUtil.java
@@ -5,11 +5,7 @@ public class ClassUtil {
         // strip off annoying cglib stuff
         if(cls.getName().contains("$$EnhancerByCGLIB")) {
             // superclass of that is what we're interested in
-            System.out.println(cls);
-            System.out.println(cls.getSuperclass());
-            System.out.println(cls.getInterfaces());
             cls = cls.getSuperclass();
-            System.out.println(cls);
         }
         return cls;
     }
